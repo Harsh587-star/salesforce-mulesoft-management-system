0b80f14c2f323d0f63f750498994c49319f2984e
Harsh raj <hr4798824@gmail.com>
2026-03-28 19:40:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Salesforce MuleSoft Management System
+
+Spring Boot project simulating CRM + integration.
+Run: mvn spring-boot:run
---
+++ src/main/java/com/project/App.java
@@ -0,0 +1,6 @@
+package com.project;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Management System Running");
+    }
+}
